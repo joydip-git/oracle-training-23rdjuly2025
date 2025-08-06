e5905ee55c55e39dd57ae31cd72be211551290c8
Joydip <joydip.mondal@gmail.com>
2025-08-06 16:59:04 +0530

    final update day-10

---
+++ 4-RESTApps/productserviceapi/src/main/java/com/oracle/productserviceapi/services/ProductService.java
@@ -0,0 +1,18 @@
+package com.oracle.productserviceapi.services;
+
+import java.util.List;
+
+import com.oracle.productserviceapi.models.ProductDTO;
+
+import jakarta.ws.rs.Path;
+
+@Path("products")
+public class ProductService {
+	public List<ProductDTO> getProducts(){
+		try {
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+}
